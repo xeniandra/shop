21922600459058ec1f180615ae06ac9f296368cb
Xeniandra <muntikas@mail.ru>
2020-04-12 17:14:50 +0300

    корзина

---
+++ js/cart.js
@@ -0,0 +1,61 @@
+var cart = {};
+
+function loadCart(){
+    //проверяю есть ли в localStorage запись cart
+    if(localStorage.getItem('cart')){
+        //если есть - расшифровываю и записываю в переменную cart
+        cart = JSON.parse(localStorage.getItem('cart'));
+		showCart(); 
+    }
+    else{
+    	$('.main-cart').html('Корзина пуста!');
+    }
+}
+
+function showCart(){
+	//вывод корзины
+        if (!isEmpty(cart)){
+        	$('.main-cart').html('Корзина пуста!');
+		}
+		else{
+	$.getJSON('goods.json', function (data) {
+		var goods = data;
+		var out = "";
+		for (var id in cart) {
+			out += `<button data-id="${id}" class="del-goods">x</button>`;
+			out += ``;
+			out += `<img src="images\\${goods[id].img}">`;
+			out += ` ${goods[id].name  } `;
+			out += ` ${cart[id]}  `;
+			out += ` <br>`;
+		}
+		$('.main-cart').html(out);
+		$('.del-goods').on('click', delGoods);
+	});
+}
+}
+
+function delGoods(){
+	//удаляем товар из корзины
+	var id = $(this).attr('data-id');
+	delete cart[id];
+	saveCart();
+	showCart();
+}
+
+
+function saveCart(){
+    //сохранение корзины в localStorage
+    localStorage.setItem('cart', JSON.stringify(cart));//коризну в строку
+}
+
+function isEmpty(object){
+	//проверка корзины на пустоту
+	for(var key in object)
+	if (object.hasOwnProperty(key)) return true;
+	return false;
+}
+
+$(document).ready(function(){
+	loadCart();
+});
