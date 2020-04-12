1df814100de9dc80aeb263f869c1e105fa997666
Xeniandra <muntikas@mail.ru>
2020-04-12 16:02:06 +0300

    Добавляем в корзину

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var cart ={};//корзина объект
+
+
 function init() {
     // файл goods.json
     $.getJSON("goods.json", goodsOut);
@@ -19,10 +22,33 @@ function goodsOut(data) {
         out +=`<p class="name">${data[key].name}</p>`;
         out +=`<img src="images/${data[key].img}" alt="">`;
         out +=`<div class="cost">${data[key].cost}</div>`;
-        out +='<button class="add-to-cart">Купить</button>';
+        out +=`<button class="add-to-cart" data-id="${key}">Купить</button>`;
         out +='</div>';
     }
     $('.goods-out').html(out);
+    $('.add-to-cart').on('click', addToCart); //при нажатии на кнопку срабатывает функция
+}
+
+function addToCart(){
+    //добавляет товар в корзину
+    var id = $(this).attr('data-id');
+    // console.log(id);
+    if (cart[id]==undefined) {
+        cart[id] = 1; //если в корзине нет товара, то делаем равным 1
+    }
+    else{
+        cart[id]++; //если такой товар есть, увеличиваю на 1
+    }
+    showMiniCart();
+}
+
+function showMiniCart(){
+    //показываю мини-корзину
+    var out="";
+    for (var key in cart) {
+        out += key +' ----- '+ cart[key]+'<br>';
+    }
+    $('.mini-cart').html(out);
 }
 
 $(document).ready(function () {
